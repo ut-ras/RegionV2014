09cdc670364af181dd1a48b65ee94ec25e9344a4
toast <ut.ieee.ras@gmail.com>
2014-02-02 17:47:37 -0600

    Initialized ros workspace

---
+++ src/insert/src/extinguish.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+
+import rospy
+import roslib
+
+
+def main():
+    rospy.init_node('extinguish')
+
+    while not rospy.is_shutdown():
+        rospy.loginfo('hello')
+        rospy.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
